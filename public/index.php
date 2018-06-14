9d6efa03321dfac0d401ef2859933d81bde5100a
永华 <18685504413@163.com>
2018-06-14 17:01:18 +0800

    完成pc端的修改

--- public/index.php
+++ public/index.php
@@ -13,6 +13,6 @@
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
-define('URL',  'http://zh.tt.com');
+define('URL',  'http://www.zhonghuanguoji.cn');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
